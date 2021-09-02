28fc0f31ae30376d845b93fa7ddef90f86fffc95
trallala9 <trallala9@o2.pl>
2021-09-02 21:48:26 +0200

    set capture size, create while loop for webcam boilerplate

--- virtual keyboard.py
+++ virtual keyboard.py
@@ -1,6 +1,11 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-#
+#increase the size, set HD resolution
 cap.set(3, 1280)
-cap.set(4, 728)
+cap.set(4, 720)
+#create a while loop(boilerplate for vebcam)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
